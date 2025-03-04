442f04790e41158e12df909da86fc324a11a0e98
你的用户名 <>
2025-03-04 23:48:43 +0800

    commit parser code

--- coding_challenge_hujing/fml_parser/src/main/java/com/fml/FmlParser.java
+++ coding_challenge_hujing/fml_parser/src/main/java/com/fml/FmlParser.java
@@ -1,5 +1,253 @@
 package com.fml;
 
+
+import cn.hutool.core.util.StrUtil;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+import java.util.StringJoiner;
+
+
 public class FmlParser {
+
+    private final static char CHARACTER_ADD = '+';
+    private final static char CHARACTER_SUBTRACT = '-';
+    private final static char CHARACTER_MULTIPLY = '*';
+    private final static char CHARACTER_DIVIDE = '/';
+    private final static char CHARACTER_LEFT_PARENTHESIS = '(';
+    private final static char CHARACTER_RIGHT_PARENTHESIS = ')';
+    private final static char CHARACTER_COMMA = ',';
+    private final static char CHARACTER_SEMICOLON = ';';
+    private final static char CHARACTER_EQUAL = '=';
+    private final static char CHARACTER_POINT = '.';
+    private final static String STR_SPACE = " ";
+    private final static String STR_EMPTY = "";
+
+    public FmlParser(String fml) {
+        this.fml = fml;
+    }
+
+    public FmlParser() {
+    }
+
+    private String fml;
+
+    public static void main(String[] args) throws Exception{
+        FmlParser fmlParser = new FmlParser("A * B + ( 1 + (C+A+B) / 4 );A = 3; B = 5; C = A + B");
+        System.out.println(fmlParser.calculate(null));
+    }
+
+    /**
+     * @param str
+     * @return java.lang.Double
+     * @throws
+     * @description 四则混合运算
+     * @author HUJING
+     * @date 2025/3/3 10:26
+     **/
+    public BigDecimal calculate(String str){
+        //1.若有入参，则以入参作为四则运算字符
+        fml = StrUtil.isNotBlank(str) ? str : fml;
+        //2.处理四则运算字符，进行分割以及变量替换
+        delExpression();
+        //3.校验四则运算表达式
+        checkExpression();
+        //4.返回计算结果
+        return getResult();
+    }
+
+    /**
+     * @description 处理字符串，得到可执行的表达式
+     * @param
+     * @return
+     * @throws
+     * @author HUJING
+     * @date 2025/3/3 11:16
+     **/
+    private void delExpression() {
+        //不包含分号，则是普通算式，快速返回 如：1 + 2 * 3
+        if(StrUtil.isBlank(fml)) return;
+        if (!fml.contains(String.valueOf(CHARACTER_SEMICOLON))) {
+            fml = fml.replaceAll(STR_SPACE, STR_EMPTY);
+            return;
+        }
+        //包含分号，则是需要进行变量替换的算式(以分号分割字符串) 如：A = 3; B = 5; C = A + B; A * B + ( 1 + C / 4 )
+        String[] strArr = fml.replaceAll(STR_SPACE, STR_EMPTY).split(String.valueOf(CHARACTER_SEMICOLON));
+        //Map存储变量数据 key=变量名，value=变量值，如key=A1,value=2
+        Map<String, String> map = new HashMap<>();
+        for (String str : strArr) {
+            //不包含等号，则是最终计算表达式 如：A * B + ( 1 + C / 4 )
+            if (!str.contains(String.valueOf(CHARACTER_EQUAL))) {
+                fml = str.replaceAll(STR_SPACE, STR_EMPTY);
+                continue;
+            }
+            //等号分割字符串
+            String[] tmp = str.split(String.valueOf(CHARACTER_EQUAL));
+            //等式左右不成立，直接丢弃 如 A= , =B , = 等
+            if (null == tmp || tmp.length < 2) {
+                continue;
+            }
+            String key = tmp[0].replaceAll(STR_SPACE, STR_EMPTY);
+            String value = tmp[1].replaceAll(STR_SPACE, STR_EMPTY);
+            //若是变量表达式，则替换表达式中所有变量为具体数字，并在前后拼接（），如：(3+5)
+            if (isExpression(value)) {
+                for (String k : map.keySet()) {
+                    value = value.replaceAll(k, map.get(k));
+                }
+                map.put(key, CHARACTER_LEFT_PARENTHESIS + value + CHARACTER_RIGHT_PARENTHESIS);
+            } else {
+                map.put(key, value);
+            }
+        }
+
+        //遍历变量数据map，将变量全量替换成数字
+        for (String key : map.keySet()) {
+            fml = fml.replaceAll(key, map.get(key));
+        }
+    }
+
+    //校验表达式是否有误
+    private void checkExpression(){
+        if(StrUtil.isBlank(fml)){
+            throw new RuntimeException("the fml is blank,please check it!");
+        }
+        char[] charArr = fml.toCharArray();
+        int rightCount=0,leftCount=0;
+        for(char cc :charArr){
+            if(CHARACTER_RIGHT_PARENTHESIS == cc) {
+                rightCount++;
+                continue;
+            }
+            if(CHARACTER_LEFT_PARENTHESIS == cc){
+                leftCount++;
+                continue;
+            }
+            if(!Character.isDigit(cc) && !isOperator(cc) && !(CHARACTER_POINT == cc)){
+                throw new RuntimeException("the fml contains illegal characters,please check it!");
+            }
+        }
+        if (rightCount != leftCount) {
+            throw new RuntimeException("the right parenthesis count is not equals left parenthesis count,please check it!");
+        }
+    }
+
+    //获取计算结果
+    private BigDecimal getResult() {
+        String operateStack = getOperateStackInfo();
+        Stack stk = new Stack();
+        String parts[] = operateStack.split(String.valueOf(CHARACTER_COMMA));
+        BigDecimal result = new BigDecimal(0);
+        for (String part : parts) {
+            char tmp = part.charAt(0);
+            //数字类型直接出栈
+            if (!isOperator(tmp)) {
+                stk.push(part);
+            } else {
+                BigDecimal a = new BigDecimal(String.valueOf(stk.pop()));
+                BigDecimal b = new BigDecimal(String.valueOf(stk.pop()));
+                result = calculate(b, a, tmp);
+                //将计算后的结果重新压入栈
+                stk.push(String.valueOf(result));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * @description 获取操作数栈信息
+     * @param
+     * @return
+     * @throws
+     * @author HUJING
+     * @date 2025/3/3 11:07
+     **/
+    private String getOperateStackInfo() {
+        Stack stk = new Stack();
+        //最终结果以逗号分割，如：1 + 2 * 3 分割成 1,2,3,*,+
+        StringJoiner joiner = new StringJoiner(String.valueOf(CHARACTER_COMMA));
+        char[] charArr = fml.toCharArray();
+        for (int i = 0; i < charArr.length; i++) {
+            char cc = charArr[i];
+            //处理整数及小数
+            if (Character.isDigit(cc) || fml.charAt(i) == CHARACTER_POINT) {
+                String tmpStr = STR_EMPTY;
+                do {
+                    tmpStr += charArr[i++];
+                } while (i < charArr.length && (Character.isDigit(charArr[i]) || fml.charAt(i) == CHARACTER_POINT));
+                i--;
+                joiner.add(tmpStr);
+            } else {
+                switch (cc) {
+                    case CHARACTER_LEFT_PARENTHESIS:
+                        stk.push(String.valueOf(cc));
+                        break;
+                    //右括号到左括号之间的数据全部出栈
+                    case CHARACTER_RIGHT_PARENTHESIS:
+                        while ((!stk.isEmpty())
+                                && (!String.valueOf(CHARACTER_LEFT_PARENTHESIS).equals(stk.peek()))) {
+                            joiner.add(String.valueOf(stk.pop()));
+                        }
+                        stk.pop();
+                        break;
+                    //加减法为同一操作级别
+                    case CHARACTER_ADD:
+                    case CHARACTER_SUBTRACT:
+                        while ((!stk.isEmpty())
+                                && (!String.valueOf(CHARACTER_LEFT_PARENTHESIS).equals(stk.peek()))) {
+                            joiner.add((String) stk.pop());
+                        }
+                        stk.push(String.valueOf(cc));
+                        break;
+                    //乘除法为同一操作级别
+                    case CHARACTER_MULTIPLY:
+                    case CHARACTER_DIVIDE:
+                        //栈顶为乘除的，直接出栈
+                        while ((!stk.empty())
+                                && ((String.valueOf(CHARACTER_MULTIPLY).equals(stk.peek()))
+                                    || (String.valueOf(CHARACTER_DIVIDE).equals(stk.peek())))) {
+                            joiner.add((String) stk.pop());
+                        }
+                        stk.push(String.valueOf(cc));
+                        break;
+                }
+            }
+        }
+
+        //将所有未出栈的数据，出栈拼接
+        while (!stk.isEmpty()) {
+            joiner.add(String.valueOf(stk.pop()));
+        }
+        return joiner.toString();
+    }
+
+    //判断是否是操作字符
+    private boolean isOperator(char op) {
+        return (op == CHARACTER_ADD || op == CHARACTER_SUBTRACT || op == CHARACTER_MULTIPLY || op == CHARACTER_DIVIDE);
+    }
+
+    //判断是否为计算表达式
+    private boolean isExpression(String str) {
+        return (StrUtil.isNotBlank(str) && (str.contains(String.valueOf(CHARACTER_ADD))
+                || str.contains(String.valueOf(CHARACTER_SUBTRACT))
+                || str.contains(String.valueOf(CHARACTER_MULTIPLY))
+                || str.contains(String.valueOf(CHARACTER_DIVIDE))));
+    }
+
+    //执行加减乘除运算
+    private BigDecimal calculate(BigDecimal a, BigDecimal b, char op) {
+        switch (op) {
+            case CHARACTER_ADD:
+                return a.add(b);
+            case CHARACTER_SUBTRACT:
+                return a.subtract(b);
+            case CHARACTER_MULTIPLY:
+                return a.multiply(b);
+            case CHARACTER_DIVIDE:
+                return a.divide(b);
+        }
+        return new BigDecimal(-1);
+    }
 
 }
--- coding_challenge_hujing/fml_parser/src/test/java/com/fml/FmlParserTest.java
+++ coding_challenge_hujing/fml_parser/src/test/java/com/fml/FmlParserTest.java
@@ -1,13 +1,194 @@
 package com.fml;
 
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
 /**
  * @program: fml-parser
- * @author:
- * @create: 2022/09/21 9:26
+ * @description
+ * test1-test3 普通表达式+ -运算，支持小数
+ * test4-test6 普通表达式*运算，乘加，乘减，支持小数
+ * test7-test9 普通表达式/运算，除加，除减，支持小数
+ * test10 普通表达式+ - * /计算
+ * test11-test15 复杂表达式+ - * /计算
+ * test16-test18 异常捕获
+ * @author: HUJING
+ * @create: 2025/03/03 12:26
  * @version: 1.0
  **/
 public class FmlParserTest {
 
+    @Test
+    public void test1() {
+        String expression = "1 + 23";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(24, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test2() {
+        String expression = "400 - 3 ";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(397, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test3() {
+        String expression = "1.5 + 2.86 - 3";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(1.36, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test4() {
+        String expression = "2 * 3";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(6, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test5() {
+        String expression = "5 *(1+2+3+(10-5+(1+2)))";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(70, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test6() {
+        String expression = "3 * ( 5.6 + 4.5 )";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(30.3, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test7() {
+        String expression = "4/2";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(2, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test8() {
+        String expression = " 3 / 4 + 5.2";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(5.95, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test9() {
+        String expression = " 12 / ( 2 + 5 -1)";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(2, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test10() {
+        String expression = " 3 / 4 + ( 5.2 -1.5 ) * 2";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(8.15, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test11() {
+        String expression = "A1 = 2; B2 = 11; A1+B2 ";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(13, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test12() {
+        String expression = "A = 3; B = 5; C = A + B; A * B + ( 1 + C / 4 )";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(18, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test13() {
+        String expression = "A = 3; B = 5; C = A * B; A * B + ( 1 + C / 4 )";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(19.75, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test14() {
+        String expression = "A * B + ( 1 + C / 4 ) - A / B; A = 3; B = 5; C = A * B";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(19.15, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test15() {
+        String expression = "A=20;B=30.5;C=22;D=500;E=D*(C/(A+B));D-A+10/5+5-2*6-3+((10+10/2)*20 -300)";
+        FmlParser fmlParser = new FmlParser(expression);
+        BigDecimal result = fmlParser.calculate(null);
+        Assert.assertEquals(472, result.doubleValue(), 0);
+        System.out.println(expression + " = " + result);
+    }
+
+    @Test
+    public void test16() {
+        //传入空字符串
+        try {
+            FmlParser fmlParser = new FmlParser();
+            fmlParser.calculate(null);
+        } catch (RuntimeException e) {
+            Assert.assertEquals("the fml is blank,please check it!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test17() {
+        //左右括号不相等
+        try {
+            String expression = "( ( 1 + 3 / 2 ) -1";
+            FmlParser fmlParser = new FmlParser(expression);
+            fmlParser.calculate(null);
+        } catch (RuntimeException e) {
+            Assert.assertEquals("the right parenthesis count is not equals left parenthesis count,please check it!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void test18() {
+        //左右括号不相等
+        try {
+            String expression = "1 + A - 0.5 ";
+            FmlParser fmlParser = new FmlParser(expression);
+            fmlParser.calculate(null);
+        } catch (RuntimeException e) {
+            Assert.assertEquals("the fml contains illegal characters,please check it!", e.getMessage());
+        }
+    }
 
 }
